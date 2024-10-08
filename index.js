5eabe3dff481dc0129d1e348d7fc5f70786052fa
navidul hoque <hnavidul@gmail.com>
2024-10-08 17:39:01 +0600

    backend

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const server = createServer(app)
 export const io = new Server(server, {
     cors: {
         origin: process.env.FRONTEND_URL,
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
 })
@@ -39,6 +40,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
 
