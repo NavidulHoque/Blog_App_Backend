dfde6aba297921f09595b55aa42f72c8db19ffca
navidul hoque <hnavidul@gmail.com>
2024-10-10 14:17:46 +0600

    backend

--- controller/auth.js
+++ controller/auth.js
@@ -69,6 +69,7 @@ export const loginAnUser = async (req, res) => {
 
         return res.cookie("token", token, { 
             httpOnly: true, 
+            secure: process.env.NODE_ENV === 'production',
             sameSite: 'Strict' ,
             maxAge: 3 * 24 * 60 * 60 * 1000
         }).json({ 
